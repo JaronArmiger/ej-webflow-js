6fe5d45bf4d24250031984326df42c5ca865f9a3
Jaron Armiger <jaron.armiger@gmail.com>
2023-04-18 15:18:59 -0700

    use ScrollTrigger

--- src/main.js
+++ src/main.js
@@ -1,1 +1,41 @@
-console.log('salut boilerplate')
+import Lenis from '@studio-freight/lenis'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+console.log('ej')
+
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+  direction: 'vertical', // vertical, horizontal
+  gestureDirection: 'vertical', // vertical, horizontal, both
+  smooth: true,
+  mouseMultiplier: 1,
+  smoothTouch: false,
+  touchMultiplier: 2,
+  infinite: false,
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+gsap.registerPlugin(ScrollTrigger)
+
+let sections = gsap.utils.toArray('.section')
+
+sections.forEach((section) => {
+  gsap.to(section, {
+    yPercent: 100,
+    ease: 'none',
+    scrollTrigger: {
+      trigger: section,
+      start: 'bottom bottom',
+      end: 'bottom top',
+      scrub: true,
+    },
+  })
+})
